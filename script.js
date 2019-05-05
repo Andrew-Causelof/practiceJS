358e8ab18b473753c50c6ef930acd9e496d8c8f8
Andrew-Causelof <akozlov@rocketmail.com>
2019-05-06 00:30:35 +0300

    Это первый комит

---
+++ script.js
@@ -0,0 +1,31 @@
+`use strict`;
+
+a = 5;
+console.log(a);
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof(answer));
+//console.log(answer);
+// console.log("arr"+ " -object");
+// console.log(4 + " -object");
+
+let incr = 10,
+    decr = 10;
+//incr++;
+//decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+     isClose = false;
+
+console.log(isChecked && isClose);     
+
+
+
